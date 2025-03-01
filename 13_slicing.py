d8af2b5b729e41fb8a50c4dfe935fe1b3ff583be
Robert Vari <mail.robertvari@gmail.com>
2025-03-01 16:32:37 +0100

    update

---
+++ 13_slicing.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+name = "Kiss Csaba"
+
+# [START:STOP:STEP]
+print(numbers[0:4])
+print(numbers[:4])
+print(numbers[4:])
+print(numbers[::2])
+print(numbers[::-1])
+print(numbers[::-2])
+print(name[::2])
+print(name[::-1].lower())
