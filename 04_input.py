ce9c392aabafc396f0c38c4911545ecb2da6fac4
Robert Vari <mail.robertvari@gmail.com>
2025-03-01 15:05:59 +0100

    update

---
+++ 04_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+address = input("Where do you live? ")
+email = input("What is your email address? ")
+
+print("Your name is", name)
+print("You live in", address)
+print("Your email is", email)
