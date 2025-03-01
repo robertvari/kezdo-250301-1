8e39c62151b4c4bc220ac7bb4b2da79de296afc4
Robert Vari <mail.robertvari@gmail.com>
2025-03-01 16:43:04 +0100

    update

--- 14_dictionaries.py
+++ 14_dictionaries.py
@@ -3,5 +3,11 @@ phonebook = {
         "name": "Kiss Csaba", 
         "address": "Budapest", 
         "email": "csaba@gmail.com"
+        },
+
+    "06 20 987 6543": {
+        "name": "Kovács Tamás", 
+        "address": "Pécs", 
+        "email": "tamas@gmail.com"
         }
 }
