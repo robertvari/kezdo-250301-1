5855a7c753b6e51b53e506ef276b9e9d551f081e
Robert Vari <mail.robertvari@gmail.com>
2025-03-01 16:49:54 +0100

    update

--- 14_dictionaries.py
+++ 14_dictionaries.py
@@ -1,13 +1,20 @@
+import pprint
+
+
 phonebook = {
     "06 20 123 4567": {
         "name": "Kiss Csaba", 
         "address": "Budapest", 
-        "email": "csaba@gmail.com"
+        "email": "csaba@gmail.com",
+        "birth_date": "1987-02-13"
         },
 
     "06 20 987 6543": {
         "name": "Kovács Tamás", 
         "address": "Pécs", 
-        "email": "tamas@gmail.com"
+        "email": "tamas@gmail.com",
+        "birth_date": "1979-03-03"
         }
 }
+
+print(phonebook["06 20 123 4567"]["birth_date"])
