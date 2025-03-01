12a797b6eced6b891d4d90b6bb77275b110b9821
Robert Vari <mail.robertvari@gmail.com>
2025-03-01 14:29:05 +0100

    update

---
+++ 01_datatypes.py
@@ -0,0 +1,24 @@
+# bool
+True
+False
+
+1
+0
+
+# strings (str)
+"Hello World"
+"Vári Róbert"
+'Hello World'
+"It's my birthday"
+# \ escape character
+'It \'s my birthday'
+
+# integers (int)
+10
+120
+-10
+-120
+
+# float
+3.14
+-3.14
