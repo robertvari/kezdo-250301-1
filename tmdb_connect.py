ec8090da490ad02851061866464913ec4a9a927d
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 16:58:48 +0100

    update

--- tmdb_connect.py
+++ tmdb_connect.py
@@ -3,8 +3,10 @@ import pprint
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
 
-poster_root = "https://image.tmdb.org/t/p/w300"
+poster_root = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
+backdrop_root = "https://media.themoviedb.org/t/p"
 
 movie = tmdb.Movies(603)
 response = movie.info()
-pprint.pprint(response)
+print(f"{poster_root}{response['poster_path']}")
+print(f"")
