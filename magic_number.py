2c438883f99a65245a7808b31a0e5de03b1a9866
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 15:33:18 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,4 +1,5 @@
-import os
+import os, random
+
 
 # Start Game Loop
 while True:
@@ -13,7 +14,11 @@ while True:
     max_number = 10
     try_count = 3
 
+    # Get a random number between min_number and max_number
+    magic_number = 5
 
+    # Get number from player
+    player_number = input("What is your guess?")
 
 
     response = input("Do you want to play again (y/n)")
