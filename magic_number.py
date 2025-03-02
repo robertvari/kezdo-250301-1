626f0edf73a5fc397a33ed00beba4f0402452939
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 15:24:44 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,9 @@
+# Start Game Loop
+while True:
+    print("-"*50, "The Magic Number Game", "-"*50)
+
+
+    response = input("Do you want to play again (y/n)")
+    if response == "n":
+        print("Game Exited. See you later!")
+        break
