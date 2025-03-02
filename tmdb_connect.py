95fcd63328f346bf9cd289adae1d5d62f804dd2e
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 16:52:15 +0100

    update

--- tmdb_connect.py
+++ tmdb_connect.py
@@ -1,2 +1,10 @@
 import tmdbsimple as tmdb
+import pprint
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
+tmdb.REQUESTS_TIMEOUT = 5
+
+poster_root = "https://image.tmdb.org/t/p/w300"
+
+movie = tmdb.Movies(603)
+response = movie.info()
+pprint.pprint(response)
