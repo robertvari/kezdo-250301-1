9bfe1b4561087958756d13ccaf7c5e7a12bd1f7d
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 17:42:37 +0100

    update

--- tmdb_connect.py
+++ tmdb_connect.py
@@ -1,20 +1,30 @@
 import tmdbsimple as tmdb
-import os
+import os, requests
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
 
 poster_root = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
 backdrop_root = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces"
 # r = raw string
-posters_local_cache = r"D:\Work\PythonSuli\kezdo-250301\tmdb_posters"
+posters_local_cache = "D:/Work/PythonSuli/kezdo-250301/tmdb_posters"
 
 
 movies = tmdb.Movies()
 popular_movies = movies.popular(page=1)
+
 for movie in popular_movies["results"]:
     print("-"*100)
     print(movie["title"])
     print(movie["overview"])
     print(movie["release_date"])
     print(movie["vote_average"])
-    print(f"{poster_root}{movie['poster_path']}")
+
+    # download poster if not exists
+    poster_file_path = f"{posters_local_cache}{movie['poster_path']}"
+    if not os.path.exists(poster_file_path):
+        poster_url = f"{poster_root}{movie['poster_path']}"
+        poster_data = requests.get(poster_url).content
+        print(f"Downloading poster: {poster_file_path}")
+
+        with open(poster_file_path, "wb") as file:
+            file.write(poster_data)
