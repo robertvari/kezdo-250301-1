5128dfefeb59c5b500e2cf021e5698a93854de08
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 17:19:00 +0100

    update

--- tmdb_connect.py
+++ tmdb_connect.py
@@ -6,3 +6,12 @@ tmdb.REQUESTS_TIMEOUT = 5
 poster_root = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
 backdrop_root = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces"
 
+movies = tmdb.Movies()
+popular_movies = movies.popular(page=1)
+for movie in popular_movies["results"]:
+    print("-"*100)
+    print(movie["title"])
+    print(movie["overview"])
+    print(movie["release_date"])
+    print(movie["vote_average"])
+    print(f"{poster_root}{movie['poster_path']}")
