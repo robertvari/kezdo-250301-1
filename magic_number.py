eb68128d55389bea871a774d6a3c7eeec78b07ea
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 15:29:38 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,6 +1,19 @@
+import os
+
 # Start Game Loop
 while True:
+    # Clear terminal
+    os.system("cls")
+
+    # Print game intro
     print("-"*50, "The Magic Number Game", "-"*50)
+
+    # Game rules
+    min_number = 1
+    max_number = 10
+    try_count = 3
+
+
 
 
     response = input("Do you want to play again (y/n)")
