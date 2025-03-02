77491bffb0441702bb43beacd3d8003d55fbb0e0
Robert Vari <mail.robertvari@gmail.com>
2025-03-02 17:04:27 +0100

    update

--- tmdb_connect.py
+++ tmdb_connect.py
@@ -6,8 +6,3 @@ tmdb.REQUESTS_TIMEOUT = 5
 poster_root = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
 backdrop_root = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces"
 
-movie = tmdb.Movies(950396)
-response = movie.info()
-pprint.pprint(response)
-print(f"{poster_root}{response['poster_path']}")
-print(f"{backdrop_root}{response['backdrop_path']}")
